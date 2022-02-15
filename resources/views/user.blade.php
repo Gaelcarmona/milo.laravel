52cf59abb2d3fa65a73fc67052131c815cb599f4
GaelGlanum <gael@glanum.com>
2022-02-15 17:09:13 +0100

    authentification

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -50,29 +50,38 @@
     </header>
     <body>
     <main>
-    <form class='mx-5 mt-5' method='post'>
-        @csrf
-        <div class='mb-3'>
-            <label for='pseudo' class='form-label fw-bold px-3'>Pseudo</label>
-            <input
-                type='text'
-                name='pseudo'
-                required
-            >
+        <div>
+            <div class="statistiques">
+                <div>
+                    <figure>
+                        <img src="">
+                    </figure>
+                </div>
+                <div>
+                    <h1>Mes statistiques</h1>
+                </div>
+            </div>
+            <div class="joueurs">
+                <div>
+                    <h1>Mes joueurs</h1>
+                </div>
+                <div>
+                    <figure>
+                        <img src="">
+                    </figure>
+                </div>
+            </div>
+            <div class="championnats">
+                <div>
+                    <figure>
+                        <img src="">
+                    </figure>
+                </div>
+                <div>
+                    <h1>Mes championnats</h1>
+                </div>
+            </div>
         </div>
-        </div>
-        <div class='mb-3'>
-            <label for='password' class='form-label fw-bold px-3'>Mot de passe</label>
-            <input
-            type='password'
-            id="password"
-            required
-            name='password'
-            value=''>
-        </div>
-        <button type='submit' class='btn btn-primary my-3'>Envoyer</button>
-    </form>
-
     </main>
     </body>
     <footer class='footer navbar bottom bg-dark  text-white py-3'>
--- routes/web.php
+++ routes/web.php
@@ -13,40 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', function () {
-    return view('accueil');
+    return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//affichage admin
-Route::get('/admin', function () {
-    return view('admin');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-Route::post('/login',[App\Http\Controllers\UserController::class, 'insert'])->name('insert.user');
-
-Route::get('/login', function () {
-    return view('login');
-});
-//Route::post('login',[App\Http\Controllers\UserController::class, 'login'])->name('login.user');
-
-//formulaire d'édition d'un user
-Route::get('/user/edit/{id}', function($id) {
-    return view('useredit',['user' => App\Models\User::where('id',$id)->first()]);
-})->name('user.edit');
-
-////update d'un user'
-Route::post('/user/update/{id}',[App\Http\Controllers\UserController::class, 'update'])->name('user.update');
-//
-////suppression d'un user'
-Route::get('/user/delete/{id}',[App\Http\Controllers\UserController::class, 'delete'])->name('user.delete');
+require __DIR__.'/auth.php';
