1e5324517c7859a43b75ce3aaff40e5c7e54eec0
Gaelcarmona <gaelcarmona.dev@gmail.com>
2022-02-27 18:29:37 +0100

    layouts, cards sur players.blade, insertion de joueur dans un champ n'affiche que les joueurs qui n'y sont pas.2

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -2,43 +2,54 @@
     <x-slot name="header">
     </x-slot>
     <main>
-        <nav aria-label="Breadcrumb" class="breadcrumb">
-            <ul>
-                <li><a href="{{ route('user') }}">Accueil</a></li>
-                <li><a href="{{ route('players') }}">Mes joueurs</a></li>
-                <li><span aria-current="page">{{ $player->pseudo }}</span></li>
-            </ul>
-        </nav>
-        <p>hello</p>
-        <p>Coucou je m'appelle {{ $player->pseudo }}</p>
-        <p>Mon id est le {{ $player->id }}</p>
-        <p>j'ai été créé le {{ $player->created_at }}</p>
-        @if($player->email == null)
-            <p>J'ai été créé par un utilisateur, je n'ai donc ni mail ni mot de passe</p>
-        @else
-            <p>mon email est {{ $player->email }}</p>
-        @endif
-        <h1>Les decks de {{ $player->pseudo }}</h1>
-        <table class='col-12 bg-main'>
-            <thead class='text-white bg-dark'>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Modifier</th>
-            <th>Supprimer</th>
-            </thead>
-            <tbody>
-            @foreach($decks as $deck)
-                <tr>
-                    <td>{{$deck->id}}</td>
-                    <td><a href="{{ route('displayDeckProfile', $deck->id) }}">{{ $deck->title}}</a></td>
-                    <td><a href="{{ route('editForm.deck', $deck->id) }}"> modifier</a></td>
-                    <td><a href="{{ route('delete.deck', $deck->id) }}" onclick="
-                        var result = alert('Vous ne pouvez pas supprimer ce deck alors que des résultats lui sont associés');return false">supprimer</a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <a href="{{ route('form.deck', ['id' => $player->id]) }}">Créer un deck</a>
+        <div class="row">
+            <div class="col-6 padding">
+                {{-- <figure> --}}
+                    <img src="../images/players.jpg" alt="">
+                {{-- </figure> --}}
+
+            </div>
+            <div class="col-6">
+                <nav aria-label="Breadcrumb" class="breadcrumb">
+                    <ul>
+                        <li><a href="{{ route('user') }}">Accueil</a></li>
+                        <li><a href="{{ route('players') }}">Mes joueurs</a></li>
+                        <li><span aria-current="page">{{ $player->pseudo }}</span></li>
+                    </ul>
+                </nav>
+                <p>hello</p>
+                <p>Coucou je m'appelle {{ $player->pseudo }}</p>
+                <p>Mon id est le {{ $player->id }}</p>
+                <p>j'ai été créé le {{ $player->created_at }}</p>
+                @if ($player->email == null)
+                    <p>J'ai été créé par un utilisateur, je n'ai donc ni mail ni mot de passe</p>
+                @else
+                    <p>mon email est {{ $player->email }}</p>
+                @endif
+                <h1>Les decks de {{ $player->pseudo }}</h1>
+                <table class='col-12 bg-main'>
+                    <thead class='text-white bg-dark'>
+                        <th>ID</th>
+                        <th>Nom</th>
+                        <th>Modifier</th>
+                        <th>Supprimer</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($decks as $deck)
+                            <tr>
+                                <td>{{ $deck->id }}</td>
+                                <td><a href="{{ route('displayDeckProfile', $deck->id) }}">{{ $deck->title }}</a></td>
+                                <td><a href="{{ route('editForm.deck', $deck->id) }}"> modifier</a></td>
+                                <td><a href="{{ route('delete.deck', $deck->id) }}"
+                                        onclick="
+                            var result = alert('Vous ne pouvez pas supprimer ce deck alors que des résultats lui sont associés');return false">supprimer</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <a href="{{ route('form.deck', ['id' => $player->id]) }}">Créer un deck</a>
+            </div>
+        </div>
     </main>
 </x-app-layout>
--- resources/views/players.blade.php
+++ resources/views/players.blade.php
@@ -8,46 +8,18 @@
                 <li><span aria-current="page">Mes joueurs</span></li>
             </ul>
         </nav>
-        <table class='col-12 bg-main'>
-            <thead class='text-white bg-dark'>
-                <th>ID</th>
-                <th>Pseudo</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ Auth::id() }}</td>
-                    <td><a href="{{ route('displayPlayerProfile', Auth::id()) }}">{{ Auth::user()->pseudo }}</a></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                @foreach ($associateUsers as $associateUser)
-                    <tr>
-                        <td>{{ $associateUser->id }}</td>
-                        <td>
-                            <a
-                                href="{{ route('displayPlayerProfile', $associateUser->id) }}">{{ $associateUser->pseudo }}</a>
-                        </td>
-                        <td><a href="{{ route('editPlayer', $associateUser->id) }}"> modifier</a></td>
-                        <td><a href="{{ route('player.delete', $associateUser->id) }}">supprimer</a></td>
-                    </tr>
-                @endforeach
-
-            </tbody>
-        </table>
-
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             @foreach ($associateUsers as $associateUser)
                 <div class="col">
                     <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                        {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
                             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
                             preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>Placeholder</title>
                             <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
                                 dy=".3em">Thumbnail</text>
-                        </svg>
+                        </svg> --}}
+                        <img src="images/players.jpg" alt="">
                         <div class="card-body">
                             <p class="card-text">{{ $associateUser->pseudo }}</p>
                             <div class="d-flex justify-content-between align-items-center">
@@ -59,7 +31,6 @@
                                     <a role="button" href="{{ route('player.delete', $associateUser->id) }}"
                                         class="btn btn-sm btn-outline-secondary">supprimer</a>
                                 </div>
-                                <small class="text-muted">9 mins</small>
                             </div>
                         </div>
                     </div>
