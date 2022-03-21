22c50953afdda8e0db9510732b329a67a97d84a5
Gaelcarmona <gaelcarmona.dev@gmail.com>
2022-03-21 15:56:13 +0100

    refonte accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,18 +2,36 @@
 <x-app-layout>
     <x-slot name="header">
     </x-slot>
-            <div class="container accueil">
-                <h1>Bienvenue</h1>
-                <p>Ce site te permet d'organiser des tournois de carte Magic entre amis.</p>
-                <p>En quelques étapes tu peux :</p>
-                <ul>
-                    <li>Créer un championnat</li>
-                    <li>Y ajouter des joueurs (tes amis trop fainéants pour s'inscrire)</li>
-                    <li>Leur attribuer des decks</li>
-                    <li>Créer des matchs et rentrer des résultats</li>
-                    <li>Profiter de toutes ces statistiques créées et envoyer le lien aux copains</li>
-                </ul>
-                <button class="btn btn-primary mb-1 mt-1 bg-info"><a href="{{ route('register') }}">Créer un compte</a></button>
-                <button class="btn btn-primary mb-1 mt-1 bg-info"><a href="{{ route('login') }}">Se connecter</a></button>
+    <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
+        <div class="card p-4">
+            <div class=" image d-flex flex-column justify-content-center align-items-center"><img
+                    src="{{ asset('images/large/158926.jpg') }}"
+                    alt=""></button>
+
+                <span><i class="fa fa-copy"></i></span>
             </div>
+
+            <div class=" padding d-flex align-items-center justify-content-center flex-column">
+                <div class=" d-flex mt-2 justify-content-center ">
+                    <div class="container accueil">
+                        <h1>Bienvenue</h1>
+                        <p>Ce site te permet d'organiser des tournois de carte Magic entre amis.</p>
+                        <p>En quelques étapes tu peux :</p>
+                        <ul>
+                            <li>Créer un championnat</li>
+                            <li>Y ajouter des joueurs</li>
+                            <li>Leur attribuer des decks</li>
+                            <li>Créer des matchs et rentrer des résultats</li>
+                            <li>Profiter de toutes ces statistiques créées et envoyer le lien aux copains</li>
+                        </ul>
+                        <button class="btn btn-primary mb-1 mt-1 bg-info"><a href="{{ route('register') }}">Créer un compte</a></button>
+                        <button class="btn btn-primary mb-1 mt-1 bg-info"><a href="{{ route('login') }}">Se connecter</a></button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+
 </x-app-layout>
