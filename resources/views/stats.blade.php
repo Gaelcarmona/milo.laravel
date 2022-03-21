70a1d337f70adf62875297d2cffe5ae1e5ef63f7
Gaelcarmona <gaelcarmona.dev@gmail.com>
2022-03-21 15:36:17 +0100

    refonte user et stats

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -3,32 +3,35 @@
     <x-slot name="header">
     </x-slot>
                 <div class="championnats row">
-                <div class="col-12 col-sm-6 padding">
-                    <figure>
+                <div class="col-12 col-md-6 px-0">
+                    <figure class="figure">
                         <img src="{{ asset('images/large/championships') }}.jpg">
+                        <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('statistic.championships') }}">Les championnats</a>
                     </figure>
                 </div>
-                <div class="col-12 col-sm-6 padding d-flex align-items-center justify-content-center flex-column">
+                <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
                     <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('statistic.championships') }}">Les championnats</a>
                 </div>
             </div>
             <div class="joueurs row">
-                <div class="col-12 col-sm-6 padding d-flex align-items-center justify-content-center flex-column">
-                    <a class="btn btn-info text-white mt-3 mb-3 text-center " href="{{ route('statistic.players') }}" >Les joueurs</a>
+                <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
+                    <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('statistic.players') }}" >Les joueurs</a>
                 </div>
-                <div class="col-12 col-sm-6 padding">
-                    <figure>
+                <div class="col-12 col-md-6 px-0">
+                    <figure class="figure">
                         <img src="{{ asset('images/large/players') }}.jpg">
+                        <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('statistic.players') }}">Les joueurs</a>
                     </figure>
                 </div>
             </div>
             <div class="decks row">
-                <div class="col-12 col-sm-6 padding">
-                    <figure>
+                <div class="col-12 col-md-6 px-0">
+                    <figure class="figure">
                         <img src="{{ asset('images/large/114446') }}.jpg">
+                        <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('statistic.decks') }}">Les decks</a>
                     </figure>
                 </div>
-                <div class="col-12 col-sm-6 padding d-flex align-items-center justify-content-center flex-column">
+                <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
                     <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('statistic.decks') }}">Les decks</a>
                 </div>
             </div>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,30 +2,37 @@
 <x-app-layout>
     <x-slot name="header">
     </x-slot>
-            <div class="joueurs row">
-                <div class="col-12 col-sm-6 padding d-flex align-items-center justify-content-center flex-column">
-                    <div>
-                        <a href="{{ route('createplayer') }}" class="btn btn-primary mt-3 mb-3 ">Créer un joueur</a>
-                        <a href="{{ route('players') }}" class="btn btn-info text-white mt-3 mb-3">Voir mes joueurs</a>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 padding">
-                    <figure>
-                        <img src="{{ asset('images/large/players') }}.jpg">
-                    </figure>
-                </div>
-            </div>
-            <div class="championnats row">
-                <div class="col-12 col-sm-6 padding">
-                    <figure>
-                        <img src="{{ asset('images/large/championships') }}.jpg">
-                    </figure>
-                </div>
-                <div class="col-12 col-sm-6 padding d-flex align-items-center justify-content-center flex-column">
-                    <div>
-                        <a href="{{route('create.championship')}}" class="btn btn-primary mt-3 mb-3">Créer un championnat</a>
-                        <a href="{{ route('championships') }}" class="btn btn-info text-white mt-3 mb-3">Voir mes championnats</a>
-                    </div>
-                </div>
-            </div>
+    <div class="decks row">
+        <div class="col-12 col-md-6 px-0">
+            <figure class="figure">
+                <img src="{{ asset('images/large/114446') }}.jpg">
+                <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('statistic.home') }}">Les statistiques</a>
+            </figure>
+        </div>
+        <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
+            <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('statistic.home') }}">Les statistiques</a>
+        </div>
+    </div>
+    <div class="joueurs row">
+        <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
+            <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('players') }}" >Mes joueurs</a>
+        </div>
+        <div class="col-12 col-md-6 px-0">
+            <figure class="figure">
+                <img src="{{ asset('images/large/players') }}.jpg">
+                <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('players') }}">Mes joueurs</a>
+            </figure>
+        </div>
+    </div>
+    <div class="championnats row">
+        <div class="col-12 col-md-6 px-0">
+            <figure class="figure">
+                <img src="{{ asset('images/large/championships') }}.jpg">
+                <a class="btn btn-info d-sm-flex d-md-none d-xl-none d-xxl-none text-white mt-3 mb-3 text-center centered" href="{{ route('championships') }}">Mes championnats</a>
+            </figure>
+        </div>
+        <div class="col-12 col-md-6 d-none d-md-flex d-flex align-items-center justify-content-center flex-column px-0">
+            <a class="btn btn-info text-white mt-3 mb-3 text-center" href="{{ route('championships') }}">Mes championnats</a>
+        </div>
+    </div>
 </x-app-layout>
